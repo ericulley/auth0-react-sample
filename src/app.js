f2e6de400ab351699f2367e3763bd2824043e365
Your Name <you@example.com>
2021-05-17 21:10:48 -0600

    added auth

--- src/app.js
+++ src/app.js
@@ -5,22 +5,25 @@ import { NavBar, Footer } from "./components";
 import { Home, Profile, ExternalApi } from "./views";
 
 import "./app.css";
+import Auth0ProviderWithHistory from "./auth0Provider";
 
 const App = () => {
   return (
-    <div id="app" className="d-flex flex-column h-100">
-      <NavBar />
-      <div className="container flex-grow-1">
-        <div className="mt-5">
-          <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/profile" component={Profile} />
-            <Route path="/external-api" component={ExternalApi} />
-          </Switch>
+    <Auth0ProviderWithHistory>
+      <div id="app" className="d-flex flex-column h-100">
+        <NavBar />
+        <div className="container flex-grow-1">
+          <div className="mt-5">
+            <Switch>
+              <Route path="/" exact component={Home} />
+              <Route path="/profile" component={Profile} />
+              <Route path="/external-api" component={ExternalApi} />
+            </Switch>
+          </div>
         </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </Auth0ProviderWithHistory>
   );
 };
 
--- src/components/main-nav.js
+++ src/components/main-nav.js
@@ -1,33 +1,55 @@
 import {NavLink} from "react-router-dom";
 import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 
-const MainNav = () => (
-  <div className="navbar-nav mr-auto">
-    <NavLink
-      to="/"
-      exact
-      className="nav-link"
-      activeClassName="router-link-exact-active"
-    >
-      Home
-    </NavLink>
-    <NavLink
-      to="/profile"
-      exact
-      className="nav-link"
-      activeClassName="router-link-exact-active"
-    >
-      Profile
-    </NavLink>
-    <NavLink
-      to="/external-api"
-      exact
-      className="nav-link"
-      activeClassName="router-link-exact-active"
-    >
-      External API
-    </NavLink>
-  </div>
-);
+const MainNav = () => {
+
+  const {loginWithRedirect, logout, user, isLoading} = useAuth0();
+
+  console.log(user)
+
+  return (
+    <div className="navbar-nav mr-auto">
+      <NavLink
+        to="/"
+        exact
+        className="nav-link"
+        activeClassName="router-link-exact-active"
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/profile"
+        exact
+        className="nav-link"
+        activeClassName="router-link-exact-active"
+      >
+        Profile
+      </NavLink>
+      <NavLink
+        to="/external-api"
+        exact
+        className="nav-link"
+        activeClassName="router-link-exact-active"
+      >
+        External API
+      </NavLink>
+      {!isLoading && !user && (
+        <button 
+          className="btn btn-primary btn-block" 
+          onClick={() => loginWithRedirect()}>
+          Log In
+        </button>
+      )}
+      {!isLoading && user && (
+        <button 
+          className="btn btn-primary btn-block" 
+          onClick={() => logout()}>
+          Log Out
+        </button>
+      )}
+    </div>
+  )
+};
 
 export default MainNav;
